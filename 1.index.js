c2ec50b5ac7428a4b2a3d3477c8b95163cbe330d
James <jhandshoe@sbcglobal.net>
2016-07-14 17:35:00 -0400

    initial commit

---
+++ 1.index.js
@@ -0,0 +1,33 @@
+/*
+var app = require('express')();
+var http = require('http').Server(app);
+
+app.get('/', function(request, response){
+	response.send('<h1>Hello World</h1>')
+});
+
+http.listen(5000, function(){
+	console.log('listening on *:5000')
+});
+*/
+
+//accessing express module
+var app = require('express')();
+
+//Using http - module that is innate to Node.js
+//('http')
+//('fs')
+var http = require('http').Server(app);
+
+//first argument is path
+//second argument is callback
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/1.ajax_example.html');
+
+});
+
+
+//listens to port # selected
+http.listen(5000, function(){
+	console.log('listening on *:5000');
+});
